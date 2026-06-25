01dfb9a84aad57b602f17d91490468a3833c72a1
AyushGhole <ayushghole@gmail.com>
2026-06-25 22:31:03 +0530

    First commit.

---
+++ pythonpart1.py
@@ -0,0 +1,41 @@
+#variables
+a=10
+b=5 
+
+#print(type(isPrime)) type and how single line comment is written in python
+''' this is a multi-line comment '''
+
+
+#Arthimetic Operators
+(a+b) #sum 
+(a-b) #subtract 
+(a*b) #multiply
+(a/b) #division 
+(a%b) #modulo 
+(a**b) #power 
+
+#Relational Operators 
+(a>b)
+(a==b) 
+(a!=b)
+(a>=b) 
+
+
+#Assignment Operators
+a=b
+a+=b
+a-=a
+a*=2
+a/=3
+
+
+#Logical Operators 
+isCheck = False 
+(not isCheck) #not
+((3>2) and (5>3)) #and 
+((3<4) or (3<2)) #or 
+
+
+#Type Conversion 
+sum=int(5 + 10.0) 
+print(type(sum))
