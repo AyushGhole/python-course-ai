c808b7c0901401ebce9e3f9463bc46a2292afe84
AyushGhole <ayushghole@gmail.com>
2026-06-26 11:51:11 +0530

    Working on python conditional statements.

--- pythonpart2.py
+++ pythonpart2.py
@@ -1,5 +1,13 @@
-age=int(input("Enter age: ")) 
-if age>18:
-  print("You can vote.")
-else:
-  print("You cannot vote.")
+
+age=int(input("Enter the age: "))  
+
+if(age >= 1 and age<=13): 
+  print("Child") 
+elif(age>13 and age<=18):
+  print("Teenager") 
+elif(age>18 and age<=60): 
+  print("Adult")  
+elif(age > 60 and age <=100): 
+  print("Senior Citizen")
+else: 
+  print("Invlid age")
