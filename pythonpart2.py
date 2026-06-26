d7af0d41dedca8a50452678e491503a34de71c46
AyushGhole <ayushghole@gmail.com>
2026-06-26 19:07:52 +0530

    Working on the conditional loops in python.

--- pythonpart2.py
+++ pythonpart2.py
@@ -1,13 +1,85 @@
+#if elif else
+# age=int(input("Enter the age: "))  
+# if(age >= 1 and age<=13): 
+#   print("Child") 
+# elif(age>13 and age<=18):
+#   print("Teenager") 
+# elif(age>18 and age<=60): 
+#   print("Adult")  
+# elif(age > 60 and age <=100): 
+#   print("Senior Citizen")
+# else: 
+#   print("Invlid age")
+# num = int(input("Enter a number: ")) 
+# if(num % 2 == 0): 
+#   print("Even") 
+# else:
+#   print("Odd")
 
-age=int(input("Enter the age: "))  
 
-if(age >= 1 and age<=13): 
-  print("Child") 
-elif(age>13 and age<=18):
-  print("Teenager") 
-elif(age>18 and age<=60): 
-  print("Adult")  
-elif(age > 60 and age <=100): 
-  print("Senior Citizen")
-else: 
-  print("Invlid age")
+# match case
+# color = input("Enter color: ")  
+# match color: 
+#   case "Red":
+#     print("Stop") 
+#   case "Yellow":
+#     print("Look") 
+#   case "Green": 
+#     print("Go")  
+#   case _: 
+#     print("Wrong input") 
+
+
+#while loop 
+# count=1 
+# while count <=5:
+#   print("Hello World!") 
+#   count += 1 
+# num=int(input("Enter number: ")) 
+# count=1 
+# while (count <=10):
+#   print(num,"x",count,"=", num*count) 
+#   count += 1 
+# name="Hello" 
+# for var in name:
+#   print(var)
+# for i in range(5):
+#   print(i+1) 
+
+# word ='artificial intellignece' 
+# ans=0
+# for ch in word: 
+#   if(ch == 'i'):
+#     ans +=1 
+# print(ans) 
+
+# word='artificial'  
+# ans=0
+# for ch in word:
+#   if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+#     ans += 1 
+# print("Vowel Count: ",ans) 
+# for i in range(2,11,2): 
+#   print(i) 
+# n=5
+# sum=0
+# for i in range(1,n+1):
+#   sum += i 
+# print(sum) 
+
+#functions 
+# def calc_avg(a,b,c):
+#   avg=(a+b+c)/3 
+#   return avg 
+# a=5
+# b=6
+# c=5 
+# print(calc_avg(a,b,c))  
+# def calc_fact(n):
+#   fact=1
+#   i=1
+#   while (i <= n):
+#     fact=fact*i 
+#     i+=1
+#   return fact 
+# print("Factorials:",calc_fact(3))
